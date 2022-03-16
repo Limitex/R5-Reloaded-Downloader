41bfde57cea3629b211e2b0667bc1268a6061327
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 19:55:43 +0900

    Create Extractor.cs

---
+++ R5-Reloaded-Downloader-Library/SevenZip/Extractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Downloader_Library.SevenZip
+{
+    public class Extractor : IDisposable
+    {
+        public Extractor()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
