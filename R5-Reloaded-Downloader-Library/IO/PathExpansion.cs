e89a466a15219b2bea77fc6afc4aa2d1d9ac05c3
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 21:38:59 +0900

    Create PathExpansion.cs

---
+++ R5-Reloaded-Downloader-Library/IO/PathExpansion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Downloader_Library.IO
+{
+    public static class PathExpansion
+    {
+        public static string Combine(params string[] paths)
+        {
+            string path = string.Empty;
+            foreach (var str in paths) path = Path.Combine(path, str);
+            return path.Replace(@"\", "/");
+        }
+    }
+}
