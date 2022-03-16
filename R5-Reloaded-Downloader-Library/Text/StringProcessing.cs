c42dd542d5dd7144c5ebdd22dab632c9d947c7ed
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-17 00:04:17 +0900

    Fixed display bug at 0

--- R5-Reloaded-Downloader-Library/Text/StringProcessing.cs
+++ R5-Reloaded-Downloader-Library/Text/StringProcessing.cs
@@ -12,6 +12,7 @@ namespace R5_Reloaded_Downloader_Library.Text
 
         public static string ByteToStringWithUnits(float data)
         {
+            if (data == 0) return "0.000".PadLeft(7) + " Byte";
             var count = 0;
             var text = new string[] { "Byte", "KB", "MB", "GB", "TB" };
             while (data >= 1024f) { data /= 1024f; count++; }
