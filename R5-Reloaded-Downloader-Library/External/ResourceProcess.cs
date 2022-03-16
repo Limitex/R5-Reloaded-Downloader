a70d15674e37e1bd98f7f83baa2acfce67acbf38
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 19:41:41 +0900

    Create ResourceProcess.cs

---
+++ R5-Reloaded-Downloader-Library/External/ResourceProcess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Downloader_Library.External
+{
+    public class ResourceProcess : IDisposable
+    {
+        public string GetPath { get; private set; }
+
+        public ResourceProcess()
+        {
+            GetPath = ExportingFile(Path.Combine(Path.GetTempPath(), "7za.dll"), "7za.dll");
+        }
+
+        public void Dispose()
+        {
+            File.Delete(GetPath);
+        }
+
+        private static string ExportingFile(string path, string resource)
+        {
+            var name = "R5_Reloaded_Downloader_Library.External.Resources." + resource;
+            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name);
+            if (stream == null) throw new("The assembly does not have the specified file.");
+            using var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+            File.WriteAllBytes(path, memoryStream.ToArray());
+            return path;
+        }
+    }
+}
