d3680fa45a622e9658b4e33ff7b7eed92244cafb
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 20:22:49 +0900

    Extractor code added

--- R5-Reloaded-Downloader-Library/SevenZip/Extractor.cs
+++ R5-Reloaded-Downloader-Library/SevenZip/Extractor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using R5_Reloaded_Downloader_Library.External;
+using SevenZip;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +10,43 @@ namespace R5_Reloaded_Downloader_Library.SevenZip
 {
     public class Extractor : IDisposable
     {
+        public event EventHandler<ProgressEventArgs>? ProgressEventReceives = null;
+
+        private ResourceProcess SevenZipDll;
+
         public Extractor()
         {
-
+            SevenZipDll = new("7za.dll");
+            SevenZipBase.SetLibraryPath(SevenZipDll.GetPath);
         }
 
         public void Dispose()
         {
+            SevenZipDll.Dispose();
+        }
 
+        public string Run(string SourceArchive, string ExtractionDirectory, string? args = null)
+        {
+            if (Directory.Exists(ExtractionDirectory) && Directory.EnumerateFileSystemEntries(ExtractionDirectory).Any())
+                throw new Exception("The extraction directory is not empty.");
+
+            var extractor = new SevenZipExtractor(SourceArchive);
+            if (ProgressEventReceives != null) extractor.Extracting += ProgressEventReceives;
+            //extractor.ExtractionFinished += (sender, args) => { };
+            extractor.ExtractArchive(ExtractionDirectory);
+
+            if (args == "f")
+            {
+                var files = Directory.GetFiles(ExtractionDirectory);
+                var dirs = Directory.GetDirectories(ExtractionDirectory);
+                if (files.Length == 0 && dirs.Length == 1)
+                {
+                    Directory.Move(dirs[0], ExtractionDirectory + "_buffer");
+                    Directory.Delete(ExtractionDirectory);
+                    Directory.Move(ExtractionDirectory + "_buffer", ExtractionDirectory);
+                }
+            }
+            return ExtractionDirectory;
         }
     }
 }
