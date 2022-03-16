d09ef9682a30a85d95ee6c31ab75924dd8a38587
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 20:31:46 +0900

    Create StringProcessing.cs

---
+++ R5-Reloaded-Downloader-Library/Text/StringProcessing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Downloader_Library.Text
+{
+    public class StringProcessing
+    {
+        public static string GetExtension(string address) => Path.GetExtension(address).Replace(".", "").ToLower();
+
+        public static string ByteToStringWithUnits(float data)
+        {
+            var count = 0;
+            var text = new string[] { "Byte", "KB", "MB", "GB", "TB" };
+            while (data >= 1024f) { data /= 1024f; count++; }
+            return data.ToString(".000").PadLeft(7) + " " + text[count];
+        }
+    }
+}
