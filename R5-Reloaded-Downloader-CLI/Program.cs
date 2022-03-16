95a3675bd8fda37ac8ed7dd3ef2fb12b86cdb1fb
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 21:35:59 +0900

    Addition of basic processing

--- R5-Reloaded-Downloader-CLI/Program.cs
+++ R5-Reloaded-Downloader-CLI/Program.cs
@@ -1,13 +1,56 @@
-﻿using R5_Reloaded_Downloader_Library.IO;
+﻿using R5_Reloaded_Downloader_Library.Get;
+using R5_Reloaded_Downloader_Library.IO;
+using R5_Reloaded_Downloader_Library.SevenZip;
+using R5_Reloaded_Downloader_Library.Text;
+using SevenZip;
 using System;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        private static string FinalDirectoryName = "R5-Reloaded";
+
         static void Main(string[] args)
         {
-            ConsoleExpansion.LogWrite("Hello, World!");
+            var DirectionPath = Path.GetDirectoryName(Environment.ProcessPath);
+            if (DirectionPath == null) throw new Exception();
+            DirectionPath = Path.Combine(DirectionPath, FinalDirectoryName);
+
+            if (!DirectoryExpansion.IsEmpty(DirectionPath))
+            {
+                ConsoleExpansion.LogError("The directory already exists.");
+                ConsoleExpansion.LogError("Move or delete the directory.");
+                ConsoleExpansion.LogError("Path : " + DirectionPath);
+                ConsoleExpansion.Exit();
+                return;
+            }
+
+            using (var download = new Download(DirectionPath))
+            {
+                download.ProgressEventReceives += HttpClientProcess_EventHandler;
+
+            }
+
+            using (var extractor = new Extractor())
+            {
+                extractor.ProgressEventReceives += Extractor_EventHandler;
+
+            }
+        }
+
+        private static void HttpClientProcess_EventHandler(long? totalFileSize, long totalBytesDownloaded, double? progressPercentage)
+        {
+            var downloadedByteSize = StringProcessing.ByteToStringWithUnits(totalBytesDownloaded);
+            var totalByteSize = StringProcessing.ByteToStringWithUnits(totalFileSize ?? 0);
+            var progressPercent = ((int?)progressPercentage ?? 0).ToString().PadLeft(2);
+            ConsoleExpansion.LogWrite($"{downloadedByteSize} / {totalByteSize} ({progressPercent}%)");
+            Thread.Sleep(100);
+        }
+
+        private static void Extractor_EventHandler(object? sender, ProgressEventArgs args)
+        {
+            ConsoleExpansion.LogWrite(args.PercentDone.ToString() + "% extracting completed.");
         }
     }
 }
