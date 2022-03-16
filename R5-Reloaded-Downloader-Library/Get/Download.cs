3300a2d5d230be81bf93ad0671cdacfeef55c8c4
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-16 19:49:34 +0900

    Create Download.cs

---
+++ R5-Reloaded-Downloader-Library/Get/Download.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5_Reloaded_Downloader_Library.Get
+{
+    public class Download : IDisposable
+    {
+        public Download()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
