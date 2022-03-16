9c7687a5a42a0120998115fd966e4b23d0e2c50d
Limitex <76650151+Limitex@users.noreply.github.com>
2022-03-17 04:48:23 +0900

    UI design

--- R5-Reloaded-Downloader-GUI/MainForm.Designer.cs
+++ R5-Reloaded-Downloader-GUI/MainForm.Designer.cs
@@ -28,21 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OfficialWebsiteLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.OfficialDiscordLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.PathSelectTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AddToStartMenuShortcutCheckBox = new System.Windows.Forms.CheckBox();
+            this.CreateDesktopShortcutCheckBox = new System.Windows.Forms.CheckBox();
+            this.FullProgressBar = new System.Windows.Forms.ProgressBar();
+            this.MonoProgressBar = new System.Windows.Forms.ProgressBar();
+            this.FullStatusLabel = new System.Windows.Forms.Label();
+            this.InstallButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "The downloader will install R5-Reloaded on your computer.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Welcome to the downloader for your R5-Reloaded.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome!";
+            // 
+            // OfficialWebsiteLinkLabel
+            // 
+            this.OfficialWebsiteLinkLabel.AutoSize = true;
+            this.OfficialWebsiteLinkLabel.Location = new System.Drawing.Point(12, 176);
+            this.OfficialWebsiteLinkLabel.Name = "OfficialWebsiteLinkLabel";
+            this.OfficialWebsiteLinkLabel.Size = new System.Drawing.Size(165, 15);
+            this.OfficialWebsiteLinkLabel.TabIndex = 8;
+            this.OfficialWebsiteLinkLabel.TabStop = true;
+            this.OfficialWebsiteLinkLabel.Text = "https://r5reloaded.gitbook.io/";
+            // 
+            // OfficialDiscordLinkLabel
+            // 
+            this.OfficialDiscordLinkLabel.AutoSize = true;
+            this.OfficialDiscordLinkLabel.Location = new System.Drawing.Point(12, 120);
+            this.OfficialDiscordLinkLabel.Name = "OfficialDiscordLinkLabel";
+            this.OfficialDiscordLinkLabel.Size = new System.Drawing.Size(215, 15);
+            this.OfficialDiscordLinkLabel.TabIndex = 9;
+            this.OfficialDiscordLinkLabel.TabStop = true;
+            this.OfficialDiscordLinkLabel.Text = "https://discord.com/invite/jqMkUdXrBr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Reference us for the official website here.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Join us for the official Discord server here.";
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(268, 234);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.TabIndex = 12;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // PathSelectTextBox
+            // 
+            this.PathSelectTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PathSelectTextBox.Location = new System.Drawing.Point(12, 235);
+            this.PathSelectTextBox.Name = "PathSelectTextBox";
+            this.PathSelectTextBox.ReadOnly = true;
+            this.PathSelectTextBox.Size = new System.Drawing.Size(250, 23);
+            this.PathSelectTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(159, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Select the directory to install.";
+            // 
+            // AddToStartMenuShortcutCheckBox
+            // 
+            this.AddToStartMenuShortcutCheckBox.AutoSize = true;
+            this.AddToStartMenuShortcutCheckBox.Checked = true;
+            this.AddToStartMenuShortcutCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AddToStartMenuShortcutCheckBox.Location = new System.Drawing.Point(17, 298);
+            this.AddToStartMenuShortcutCheckBox.Name = "AddToStartMenuShortcutCheckBox";
+            this.AddToStartMenuShortcutCheckBox.Size = new System.Drawing.Size(121, 19);
+            this.AddToStartMenuShortcutCheckBox.TabIndex = 13;
+            this.AddToStartMenuShortcutCheckBox.Text = "Add to start menu";
+            this.AddToStartMenuShortcutCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CreateDesktopShortcutCheckBox
+            // 
+            this.CreateDesktopShortcutCheckBox.AutoSize = true;
+            this.CreateDesktopShortcutCheckBox.Checked = true;
+            this.CreateDesktopShortcutCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CreateDesktopShortcutCheckBox.Location = new System.Drawing.Point(17, 273);
+            this.CreateDesktopShortcutCheckBox.Name = "CreateDesktopShortcutCheckBox";
+            this.CreateDesktopShortcutCheckBox.Size = new System.Drawing.Size(160, 19);
+            this.CreateDesktopShortcutCheckBox.TabIndex = 14;
+            this.CreateDesktopShortcutCheckBox.Text = "Create a desktop shortcut";
+            this.CreateDesktopShortcutCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // FullProgressBar
+            // 
+            this.FullProgressBar.Location = new System.Drawing.Point(12, 360);
+            this.FullProgressBar.Name = "FullProgressBar";
+            this.FullProgressBar.Size = new System.Drawing.Size(331, 23);
+            this.FullProgressBar.TabIndex = 17;
+            // 
+            // MonoProgressBar
+            // 
+            this.MonoProgressBar.Location = new System.Drawing.Point(12, 331);
+            this.MonoProgressBar.Name = "MonoProgressBar";
+            this.MonoProgressBar.Size = new System.Drawing.Size(331, 23);
+            this.MonoProgressBar.TabIndex = 16;
+            // 
+            // FullStatusLabel
+            // 
+            this.FullStatusLabel.Location = new System.Drawing.Point(12, 386);
+            this.FullStatusLabel.Name = "FullStatusLabel";
+            this.FullStatusLabel.Size = new System.Drawing.Size(331, 68);
+            this.FullStatusLabel.TabIndex = 15;
+            this.FullStatusLabel.Text = "...";
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.Location = new System.Drawing.Point(268, 428);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(75, 23);
+            this.InstallButton.TabIndex = 18;
+            this.InstallButton.Text = "Install";
+            this.InstallButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(355, 463);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.FullProgressBar);
+            this.Controls.Add(this.MonoProgressBar);
+            this.Controls.Add(this.FullStatusLabel);
+            this.Controls.Add(this.AddToStartMenuShortcutCheckBox);
+            this.Controls.Add(this.CreateDesktopShortcutCheckBox);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.PathSelectTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.OfficialWebsiteLinkLabel);
+            this.Controls.Add(this.OfficialDiscordLinkLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "R5 Reloaded Downloader";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        public LinkLabel OfficialWebsiteLinkLabel;
+        public LinkLabel OfficialDiscordLinkLabel;
+        private Label label5;
+        private Label label4;
+        public Button BrowseButton;
+        public TextBox PathSelectTextBox;
+        private Label label6;
+        public CheckBox AddToStartMenuShortcutCheckBox;
+        public CheckBox CreateDesktopShortcutCheckBox;
+        public ProgressBar FullProgressBar;
+        public ProgressBar MonoProgressBar;
+        public Label FullStatusLabel;
+        public Button InstallButton;
     }
 }
